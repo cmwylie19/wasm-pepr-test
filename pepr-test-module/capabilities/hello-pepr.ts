ab9f1b87615b7ba789720767a6badb8860404872
Case Wylie <cmwylie19@defenseunicorns.com>
2023-10-12 21:37:11 -0400

    init
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

---
+++ pepr-test-module/capabilities/hello-pepr.ts
@@ -0,0 +1,64 @@
+import {
+  Capability,
+  K8s,
+  Log,
+  PeprMutateRequest,
+  RegisterKind,
+  a,
+  fetch,
+  fetchStatus,
+  kind,
+} from "pepr";
+import { readFileSync } from "fs";
+
+/**
+ *  The HelloPepr Capability is an example capability to demonstrate some general concepts of Pepr.
+ *  To test this capability you run `pepr dev`and then run the following command:
+ *  `kubectl apply -f capabilities/hello-pepr.samples.yaml`
+ */
+export const HelloPepr = new Capability({
+  name: "hello-pepr",
+  description: "A simple example capability to show how things work.",
+  namespaces: ["pepr-demo", "pepr-demo-2"],
+});
+
+// Use the 'When' function to create a new action, use 'Store' to persist data
+const { When, Store } = HelloPepr;
+
+async function processTemplate(a,b) {
+  const go = new globalThis.Go();
+
+  const wasmData = readFileSync("main.wasm");
+  var result: string = undefined;
+
+  await WebAssembly.instantiate(wasmData, go.importObject).then(wasmModule => {
+    go.run(wasmModule.instance);
+  
+    result = global.peprWasm(a,b);
+  });
+  return result;
+}
+
+/**
+ * ---------------------------------------------------------------------------------------------------
+ *                                   Mutate Action (Namespace)                                   *
+ * ---------------------------------------------------------------------------------------------------
+ *
+ * This action removes the label `remove-me` when a Namespace is created.
+ * Note we don't need to specify the namespace here, because we've already specified
+ * it in the Capability definition above.
+ */
+When(a.Pod)
+.IsCreated()
+.Mutate(async pod => {
+
+
+  try {
+    let label_value = await processTemplate("a","b")
+    pod.SetLabel("pepr",label_value)
+
+  } 
+  catch(err) {
+    Log.error(err);
+  }
+});
---
+++ wasm/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+func concats(this js.Value, args []js.Value) interface{} {
+
+	strongOne := args[0].String()
+	stringTwo := args[1].String()
+	return fmt.Sprintf("%s%s", strongOne, stringTwo)
+}
+
+func main() {
+	done := make(chan struct{}, 0)
+	// https://github.com/golang/go/issues/25612
+	js.Global().Set("peprWasm", js.FuncOf(concats))
+	<-done
+}
